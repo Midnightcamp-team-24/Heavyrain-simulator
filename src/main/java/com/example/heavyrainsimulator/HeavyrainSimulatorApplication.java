3d7b8f7abbe40bba57f375e4c524037c8067d837
anichan <qufekf97@gmail.com>
2023-08-30 21:46:30 +0900

    gradle 변경

---
+++ src/main/java/com/example/heavyrainsimulator/HeavyrainSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.heavyrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeavyrainSimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeavyrainSimulatorApplication.class, args);
+    }
+
+}
